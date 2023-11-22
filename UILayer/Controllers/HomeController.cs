ab201dc4cf69ff0917d227e6a6311e759d9daccf
raj <raj@gmail.com>
2023-11-22 15:15:00 +0530

    addednewfeature

--- UILayer/Controllers/HomeController.cs
+++ UILayer/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace UILayer.Controllers
             return View();
         }
 
+        public ActionResult portal()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
